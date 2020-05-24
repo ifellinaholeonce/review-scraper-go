ea6b1459682e2f8d7faf323ca4b4ca18d7b14b41
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-05-23 22:42:55 -0400

    start shopify search

--- parse.go
+++ parse.go
@@ -23,14 +23,29 @@ func Parse(reader io.Reader) ([]Review, error) {
 		return nil, err
 	}
 	dfs(doc, "")
+	// 1. Find all the .review-listing divs
+	// 2. For each review-listing node
+	//   2.a get the date
+	//   2.b get the rating
+	//   2.c get the title
+	//   2.d get the text
+	// 3. return the Reviews
 	return nil, nil
+}
+
+func shopifyReviewListing(node *html.Node) []*html.Node {
+	if node.Type == html.ElementNode && node.Data == "div" {
+		return []*html.Node{node}
+	}
+	var ret []*html.Node
+	return ret
 }
 
 func dfs(node *html.Node, padding string) {
 	msg := node.Data
-	if node.Type == html.ElementNode {
-		msg = "<" + msg + ">"
-	}
+	// if node.Type == html.ElementNode {
+	// 	msg = "<" + msg + ">"
+	// }
 	fmt.Println(padding, msg)
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
 		dfs(child, padding+"  ")
