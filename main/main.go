992d532e19f356e8329e5f381eb59afde908fa97
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-06-06 23:23:46 -0400

    find or create app

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"review-scraper-go/appscrape"
@@ -14,11 +15,14 @@ func main() {
 
 	var appName string
 	var pageCount int
+
 	fmt.Println("What is the name of the app?")
 	_, err := fmt.Scan(&appName)
 	if err != nil {
 		panic(err)
 	}
+	appID := findOrCreateApp(appName)
+
 	fmt.Println("How many pages? (0 for all pages)")
 	_, err = fmt.Scan(&pageCount)
 	if err != nil {
@@ -27,28 +31,62 @@ func main() {
 
 	reviews := appscrape.Scrape(appName, pageCount)
 	if len(reviews) > 0 {
-		success := saveReviewRecords(reviews)
+		success := saveReviewRecords(reviews, appID)
 		if !success {
 			log.Fatal("Failed to persist review records")
 		}
 	}
 }
 
-func saveReviewRecords(records []pageparse.Review) bool {
-	err := db.Conn.Ping()
-	if err != nil {
-		panic(err)
-	}
-
+func saveReviewRecords(records []pageparse.Review, appID int) bool {
 	for _, record := range records {
 		sqlStatement := `
-		INSERT INTO reviews (rating, title, text)
-		VALUES ($1, $2, $3)`
+		INSERT INTO reviews (rating, title, text, app_id)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id`
 
-		_, err := db.Conn.Exec(sqlStatement, record.Rating, record.Title, record.Text)
+		_, err := db.Conn.Exec(
+			sqlStatement,
+			record.Rating,
+			record.Title,
+			record.Text,
+			appID)
 		if err != nil {
 			panic(err)
 		}
 	}
 	return true
+}
+
+func findOrCreateApp(appName string) int {
+	var id int
+
+	sqlStatement := `SELECT id FROM apps WHERE name ILIKE $1`
+
+	row := db.Conn.QueryRow(sqlStatement, appName)
+
+	switch err := row.Scan(&id); err {
+	case sql.ErrNoRows:
+		fmt.Println("App not found, creating app record.")
+		id = createAppRecord(appName)
+	case nil:
+		fmt.Println(id)
+	default:
+		panic(err)
+	}
+	return id
+}
+
+func createAppRecord(appName string) int {
+	var id int
+
+	sqlStatement := `
+		INSERT INTO apps (name)
+		VALUES($1)
+		RETURNING id`
+	err := db.Conn.QueryRow(sqlStatement, appName).Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+	return id
 }
